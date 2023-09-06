e087afbe80cc1cf2a46c6ee61a4ba942206f8d60
Diego Lafuente <diegolafuente14@hotmail.com>
2023-09-06 18:37:11 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,65 +1,48 @@
-const elementos = [
-    { nombre: "Proteina chocolate", valor: 2500},
-    { nombre: "Proteina vainilla", valor: 2600},
-    { nombre: "Proteina frutilla", valor: 2400},
-    { nombre: "Creatina 350g", valor: 1500},
-    { nombre: "Creatina 500g", valor: 1400},
+const productos = [
+    { id: 1, nombre: "Proteina chocolate", precio: 2500, img: "imagenes/prote choco.jpg"},
+    { id: 2, nombre: "Proteina vainilla", precio: 2600, img: "imagenes/prote vainilla.jpg"},
+    { id: 3, nombre: "Proteina frutilla", precio: 2400, img: "imagenes/prote frutilla.png"},
+    { id: 4, nombre: "Creatina 350g", precio: 1500, img: "imagenes/creatina 350.webp"},
+    { id: 5, nombre: "Creatina 500g", precio: 1400, img: "imagenes/creatina 500g.webp"},
 ];
 
-console.log (elementos);
+const botonBuscar = document.getElementById("btnBuscar");
+const inputBusqueda = document.getElementById("inputBusqueda");
+const contenedorResultados = document.getElementById("contenedorResultados");
 
-alert ("Bienvenido/a a Tiendafit, a continuacion se le mostraran los productos disponibles");
+botonBuscar.addEventListener("click", () => {
+    const terminoBusqueda = inputBusqueda.value.trim().toLowerCase();
+    const productosEncontrados = productos.filter((producto) =>
+        producto.nombre.toLowerCase().includes(terminoBusqueda)
+    );
 
-let  Productos = parseFloat (prompt("indique del 1 al 5 que producto quiere comprar : \n 1- Proteina Chocolate $2500 \n 2- Proteina vainilla $2600 \n 3- Proteina frutilla $2400 \n 4- Creatina 350g $1500 \n 5- Creatina 500g $1400 \n Para confirmar la compra o salir apriete 0")); 
+    if (productosEncontrados.length === 0) {
+        contenedorResultados.textContent = "No se encontraron productos.";
+    } else {
+        contenedorResultados.innerHTML = ""; 
 
-let seleccionarUnidades;
+        productosEncontrados.forEach((producto) => {
+            const productoDiv = document.createElement("div");
+            productoDiv.classList.add("producto");
+            
+            const imagen = document.createElement("img");
+            imagen.src = producto.img;
+            imagen.alt = producto.nombre;
+            imagen.classList.add("imagen-pequena");
+            
+            const nombre = document.createElement("h2");
+            nombre.textContent = producto.nombre;
+            
+            const precio = document.createElement("p");
+            precio.textContent = `Precio: $${producto.precio}`;
+            
+            productoDiv.appendChild(imagen);
+            productoDiv.appendChild(nombre);
+            productoDiv.appendChild(precio);
+            
+            contenedorResultados.appendChild(productoDiv);
+        });
 
-let total = 0;
-
-let arrayProductos = []
-
-const cantidad = (cant, precio) => {
-    return cant * precio
-};
-
-while (Productos != 0) {	
-    switch (Productos) {
-        case 1 :
-            seleccionarUnidades = parseFloat(prompt("Proteina chocolate - indique la cantidad que desea llevar de este producto"))
-                total += cantidad (seleccionarUnidades, 2500)
-                arrayProductos.push(`Proteina chocolate, ${seleccionarUnidades}, ${total}`)
-            break;
-        case 2 :
-            seleccionarUnidades = parseFloat(prompt("Proteina vainilla - indique la cantidad que desea llevar de este producto"))
-                total += cantidad (seleccionarUnidades, 2600)
-                arrayProductos.push(`Proteina vainilla, ${seleccionarUnidades}, ${total}`)
-            break;
-        case 3 :
-            seleccionarUnidades = parseFloat(prompt("Proteina frutilla - indique la cantidad que desea llevar de este producto"))
-                total += cantidad (seleccionarUnidades, 2400)
-                arrayProductos.push(`Proteina frutilla, ${seleccionarUnidades}, ${total}`)
-            break;
-        case 4 :
-            seleccionarUnidades = parseFloat(prompt("Creatina 350g  - indique la cantidad que desea llevar de este producto"))
-                total += cantidad (seleccionarUnidades, 1500)
-                arrayProductos.push(`Creatina 350g, ${seleccionarUnidades}, ${total}`)
-            break;
-        case 5 :
-            seleccionarUnidades = parseFloat(prompt("Creatina 500g - indique la cantidad que desea llevar de este producto"))
-                total += cantidad (seleccionarUnidades, 1400)
-                arrayProductos.push(`Creatina 500g, ${seleccionarUnidades}, ${total}`)
-            break;
-        default :	
-            break;
+        localStorage.setItem("productosEncontrados", JSON.stringify(productosEncontrados));
     }
-
-    Productos = parseFloat (prompt("indique del 1 al 5 que producto quiere comprar : \n 1- Proteina Chocolate $2500 \n 2- Proteina vainilla $2600 \n 3- Proteina frutilla $2400 \n 4- Creatina 350g $1500 \n 5- Creatina 500g $1400 \n Para confirmar la compra o salir apriete 0"))
-};
-
-console.log(arrayProductos)
-
-console.log ('El total de la compra es : $' + total)
-
-alert ("El total de la compra es : $" + total)
-
-alert ("Gracias por confiar en nosotros!!")
+});
